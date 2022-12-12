41c03a431302b4e307bc20376f2005c09090f943
André da Maia <andre.jr91@gmail.com>
2022-12-12 16:48:34 -0300

    ajustes

--- src/scripts/pages/residencial-marselha-landing-page.js
+++ src/scripts/pages/residencial-marselha-landing-page.js
@@ -8,9 +8,21 @@ if(pageurl == 'residencial-marselha-landing-page'){
   const scroll = new Scroll()
 
     var swiperlanding = new Swiper(".landing-area-swiper", {
+    slidesPerView: 3,
+    spaceBetween: 0,
+    centeredSlides: true,
+    breakpoints: {
+      // when window width is >= 320px
+      320: {
+        slidesPerView: 1,
+        spaceBetween: 0,
+      },
+
+      822: {
         slidesPerView: 3,
         spaceBetween: 0,
-        centeredSlides: true,
+      },
+  },
   });
 
 }
